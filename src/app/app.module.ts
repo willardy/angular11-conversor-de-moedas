4d98ed4a9d07bfeca854ca727c9392e59adc9e02
willardy.oliveira <willardy.oliveira@unimedteresina.com.br>
2020-12-10 14:20:59 -0300

    - Criando modulo de conversor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+/* Modules */
+import { AppRoutingModule } from './app-routing.module';
+import { ConversorModule } from './conversor';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ConversorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
