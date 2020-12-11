0dd3fa8d7eb0450081e76903ba98d28dc8751a18
willardy.oliveira <willardy.oliveira@unimedteresina.com.br>
2020-12-11 10:02:45 -0300

    Criado o service de moeda

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ConversorComponent } from './components';
+import { MoedaService } from './services';
 
 @NgModule({
   declarations: [ConversorComponent],
@@ -10,6 +11,9 @@ import { ConversorComponent } from './components';
   ],
   exports: [
     ConversorComponent
+  ],
+  providers: [
+    MoedaService
   ]
 })
 export class ConversorModule { }
